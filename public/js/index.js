ab74a04f4578be08ed7945692d90a658c3d4d078
bielnzar <biel.nizar79@gmail.com>
2024-10-11 22:33:34 +0700

    update

--- public/js/index.js
+++ public/js/index.js
@@ -6,3 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
         navLinks.classList.toggle('show');
     });
 });
+
+// Backgorund Hero
+document.addEventListener("DOMContentLoaded", function() {
+    const heroBackground = document.getElementById('hero-background');
+    const backgrounds = [
+        "/pweb-js-P29-2024/public/images/back-1.jpg",
+        "/pweb-js-P29-2024/public/images/back-2.jpg",
+        "/pweb-js-P29-2024/public/images/back-3.jpg"
+    ];
+    let currentIndex = 0;
+
+    function changeBackground(index) {
+        currentIndex = (index + backgrounds.length) % backgrounds.length;
+        heroBackground.src = backgrounds[currentIndex];
+    }
+
+    function nextBackground() {
+        changeBackground(currentIndex + 1);
+    }
+
+    function prevBackground() {
+        changeBackground(currentIndex - 1);
+    }
+
+    setInterval(nextBackground, 5000);
+
+    window.nextBackground = nextBackground;
+    window.prevBackground = prevBackground;
+});
